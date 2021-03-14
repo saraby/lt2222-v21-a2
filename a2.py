d7c885a8aac0d6025f91614fd7e15f42270109f5
saraby <sarab.youssef@gmail.com>
2021-03-14 23:49:17 +0100

    initial submition

--- a2.py
+++ a2.py
@@ -1,9 +1,18 @@
+from sklearn.decomposition import TruncatedSVD
+from sklearn.metrics import confusion_matrix as cm
+from collections import Counter
+from nltk.corpus import wordnet as wn
 import sys
 import os
 import numpy as np
 import numpy.random as npr
 import pandas as pd
 import random
+import string
+from nltk.stem import WordNetLemmatizer
+import nltk
+nltk.download('wordnet')
+
 
 # Module file with functions that you fill in so that they can be
 # called by the notebook.  This file should be in the same
@@ -15,10 +24,42 @@ import random
 # code with reasonable comments.
 
 # Function for Part 1
+
+
 def preprocess(inputfile):
-    return inputfile.readlines()
+    wnl = WordNetLemmatizer()
+    tags = ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
+    columns_names = ["Line_No", "Sentence_No", "Word", "POS", "NER"]
+    text_list = []
+    # freq_words=[]
+    # chosen_words=[]
+
+    for row in inputfile.readlines()[1:]:
+        row_items = row.rstrip().split("\t")
+        row_items[0] = int(row_items[0])
+        row_items[1] = int(float(row_items[1]))
+        if row_items[3].isalpha():
+            if row_items[3] not in tags:
+                row_items[2] = wnl.lemmatize(row_items[2].lower())
+            else:
+                row_items[2] = wn._morphy(row_items[2].lower(), pos='v')
+                verb = str(row_items[2]).strip("['").strip("']")
+                row_items[2] = verb
+            # freq_words.append(row_items[2])
+            text_list.append(row_items)
+    # most_freq = Counter(freq_words).most_common()
+    # chosen_words = [i for (i,j) in most_freq if j > 1]
+    # # print(chosen_words[4:10])
+    # for row in text_list:
+    #     if row[2] not in chosen_words:
+    #         text_list.pop(text_list.index(row))
+    fd = pd.DataFrame(text_list, columns=columns_names)
+    # fd.set_index("Line_No")
+    return fd
 
 # Code for part 2
+
+
 class Instance:
     def __init__(self, neclass, features):
         self.neclass = neclass
@@ -30,40 +71,100 @@ class Instance:
     def __repr__(self):
         return str(self)
 
+
 def create_instances(data):
+    data_list = data.values.tolist()
+
+    savior_list = [[0, 0, "", "", ""]]*5
+    for l in savior_list:
+        data_list.append(l)
+        data_list.insert(0, l)
+
     instances = []
-    for i in range(100):
-        neclass = random.choice(['art','eve','geo','gpe','nat','org','per','tim'])
-        features = ["something"] * random.randint(1, 10)
-        instances.append(Instance(neclass, features))
+    for i, sent in enumerate(data_list):
+        features = []
+        pre_features = []
+        post_features = []
+        start_tokens = ["<S5>", "<S4>", "<S3>", "<S2>", "<S1>"]
+        end_tokens = ["</S5>", "</S4>", "</S3>", "</S2>", "</S1>"]
+
+        index = i + 5
+        if sent[4].startswith("B"):
+
+            neclass = sent[4][2:]
+            for n in range(1, 6):
+                pre_row = data_list[index-n]
+                embedded_word = embedd(
+                    sent[1], pre_row[1], pre_row[4], pre_row[2], start_tokens)
+                pre_features.insert(0, embedded_word)
+
+            skipped = 1
+            while data_list[index+skipped][4].startswith('I'):
+                skipped = skipped+1
+            for m in range(1, 6):
+                if (len(data_list)-index) >= 5:
+                    post_row = data_list[index+skipped+m-1]
+                    embedded_word = embedd(
+                        sent[1], post_row[1], post_row[4], post_row[2], end_tokens)
+                    post_features.append(embedded_word)
+
+            features = pre_features + post_features
+
+            instances.append(Instance(neclass, features))
     return instances
 
+
+def embedd(sent_no, feat_sent_no, ner, word, tokens):
+    if sent_no == feat_sent_no and ner == "O" and len(tokens) == 5:
+        return word
+    else:
+        return tokens.pop(0)
+
 # Code for part 3
+
+
 def create_table(instances):
-    df = pd.DataFrame()
-    df['class'] = [random.choice(['art','eve','geo','gpe','nat','org','per','tim']) for i in range(100)]
-    for i in range(3000):
-        df[i] = npr.random(100)
+
+    extracted_features = [
+        feature for instance in instances for feature in instance.features]
+    most_freq = Counter(extracted_features).most_common(3000)
+    top_freq = [i for (i, j) in most_freq]
+    cols = ["Class"] + top_freq
+    rows = [[instance.neclass]+[feature.count(word) for word in top_freq]
+            for instance in instances for feature in instance.features]
+
+    df = pd.DataFrame(rows, columns=cols)
+    reducted_df = reduce(df.drop('Class', 1))
+    reducted_df.insert(0, "Class", df["Class"])
+
+    return reducted_df
+
+
+def reduce(matrix, dims=300):
+    svd = TruncatedSVD(n_components=dims)
+    matrix_reduced = svd.fit_transform(matrix)
+    matrix_df = pd.DataFrame(matrix_reduced, index=matrix.index)
+    return matrix_df
+
+
+def ttsplit(bigdf):
+
+    df_train = bigdf.sample(frac=0.8).reset_index()
+    df_test = bigdf.drop(df_train.index).reset_index()
+
+    return df_train.drop('Class', axis=1).to_numpy(), df_train['Class'], df_test.drop('Class', axis=1).to_numpy(), df_test['Class']
+
+# Code for part 5
+
+
+def confusion_matrix(truth, predictions):
+
+    labels = list(np.unique(truth.T))
+    confusion_matrix = cm(truth, predictions, labels=labels)
+    df = pd.DataFrame(confusion_matrix, index=labels, columns=labels)
 
     return df
 
-def ttsplit(bigdf):
-    df_train = pd.DataFrame()
-    df_train['class'] = [random.choice(['art','eve','geo','gpe','nat','org','per','tim']) for i in range(80)]
-    for i in range(3000):
-        df_train[i] = npr.random(80)
-
-    df_test = pd.DataFrame()
-    df_test['class'] = [random.choice(['art','eve','geo','gpe','nat','org','per','tim']) for i in range(20)]
-    for i in range(3000):
-        df_test[i] = npr.random(20)
-        
-    return df_train.drop('class', axis=1).to_numpy(), df_train['class'], df_test.drop('class', axis=1).to_numpy(), df_test['class']
-
-# Code for part 5
-def confusion_matrix(truth, predictions):
-    print("I'm confusing.")
-    return "I'm confused."
 
 # Code for bonus part B
 def bonusb(filename):
